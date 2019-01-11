5cc08a75dcd56932fea65eed53614d09942b432f
Stephanie Abud <sabud1208@gmail.com>
2019-01-11 19:36:26 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,11 @@
 # code here!
+class School
+  attr_accessor :roster
+ def initialize(roster)
+   @roster = {}
+ end
+ 
+ def add_student(name, grade)
+  @roster << roster[grade] = [name]
+ end
+end
